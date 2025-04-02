1c42f2bf2785e3782910f836742d43d7bf16bdd6
Natali Ustinova <nataliaustinova58@gmail.com>
2025-04-02 12:05:58 +0300

    feat: ffmpeg builder

---
+++ src/commands/ffmpeg/ffmpeg.builder.ts
@@ -0,0 +1,33 @@
+//Builder pattern
+export class FFmpegBuilder {
+  private inputPath: string;
+  private options: Map<string, string> = new Map();
+  
+  constructor() {
+    this.options.set('-c:v', 'libx264')
+  }
+  
+  input(inputPath: string): this {
+    this.inputPath = inputPath;
+    return this;
+  }
+  
+  setVideoSize(width: number, height: number): this {
+    this.options.set('-s', `${width}x${height}`)
+    return this;
+  }
+  
+  output(outputPath: string) {
+    if (!this.inputPath) {
+      throw new Error('No input set')
+    }
+    const args: string[] = ['-i', this.inputPath]
+    this.options.forEach((key, value) => {
+      args.push(key)
+      args.push(value)
+    })
+    args.push(outputPath);
+    return args;
+  }
+}
+
