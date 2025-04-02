6d2c9f22db00e628880263025f31c198e200aae8
Natali Ustinova <nataliaustinova58@gmail.com>
2025-04-02 11:44:24 +0300

    feat: Template method for executor

---
+++ src/core/executor/command.executor.ts
@@ -0,0 +1,21 @@
+import { IStreamLogger } from "../handlers/strem-logger.interface";
+import {ChildProcessWithoutNullStreams, spawn} from "node:child_process";
+import {ICommandExec} from "./command.types";
+//Template method for executor
+export abstract class CommandExecutor<Input> {
+  constructor(private logger: IStreamLogger) {}
+  
+  public async execute() {
+    const input = await this.prompt();
+    const command = this.build(input);
+    const stream = this.spawn(command);
+    this.processStream(stream, this.logger);
+  }
+  
+  protected abstract prompt(): Promise<Input>;
+  protected abstract build(input: Input): ICommandExec; 
+  protected abstract spawn(command: ICommandExec): ChildProcessWithoutNullStreams;
+  protected abstract processStream(stream: ChildProcessWithoutNullStreams, logger: IStreamLogger): void;
+}
+
+spawn('ffmpeg', ['-i', 'ss'])
